24d08578525342875f0b6e6d783318128b855c30
Thomas Gratier <thomas_gratier@yahoo.fr>
2022-02-22 00:15:25 +0100

    Prepare sources to download OSM data only using env variable

--- prepare-sources.js
+++ prepare-sources.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+require('dotenv').config()
 const path = require('path')
 const fs = require('fs')
 const {pathExists, mkdirp} = require('fs-extra')
@@ -11,8 +12,11 @@ const SOURCES_PATH = path.join(__dirname, 'sources')
 const ADMIN_EXPRESS_BASE_URL = 'http://files.opendatarchives.fr/professionnels.ign.fr/adminexpress/'
 const ADMIN_EXPRESS_FILE = 'ADMIN-EXPRESS-COG_3-0__SHP__FRA_WM_2021-05-19.7z'
 
-const OSM_COMMUNES_COM_BASE_URL = 'https://osm13.openstreetmap.fr/~cquest/openfla/export/'
+const OSM_COMMUNES_BASE_URL = 'https://osm13.openstreetmap.fr/~cquest/openfla/export/'
+const OSM_COMMUNES_FILE = 'communes-20220101-shp.zip'
 const OSM_COMMUNES_COM_FILE = 'communes-com-20220101-shp.zip'
+
+const DATASOURCES_TYPE = process.env.DATASOURCES_TYPE || 'admin-express'
 
 function getSourceFilePath(fileName) {
   return path.join(SOURCES_PATH, fileName)
@@ -50,16 +54,16 @@ async function decompressAdminExpressFiles() {
   })
 }
 
-async function decompressOsmCommunesComFiles() {
+async function decompressOsmCommunesComFiles(osm_file, file_pattern) {
   await decompress(
-    getSourceFilePath(OSM_COMMUNES_COM_FILE),
+    getSourceFilePath(osm_file),
     SOURCES_PATH,
     {
       filter(file) {
-        return file.path.startsWith('communes-com')
+        return file.path.startsWith(file_pattern)
       },
       map(file) {
-        file.path = 'osm-communes-com' + path.extname(file.path)
+        file.path = 'osm-' + file_pattern + path.extname(file.path)
         return file
       }
     }
@@ -68,10 +72,15 @@ async function decompressOsmCommunesComFiles() {
 
 async function main() {
   await mkdirp(SOURCES_PATH)
-  await downloadSourceFile(ADMIN_EXPRESS_BASE_URL + ADMIN_EXPRESS_FILE, ADMIN_EXPRESS_FILE)
-  await decompressAdminExpressFiles()
-  await downloadSourceFile(OSM_COMMUNES_COM_BASE_URL + OSM_COMMUNES_COM_FILE, OSM_COMMUNES_COM_FILE)
-  await decompressOsmCommunesComFiles()
+  if (DATASOURCES_TYPE == 'admin-express') {
+    await downloadSourceFile(ADMIN_EXPRESS_BASE_URL + ADMIN_EXPRESS_FILE, ADMIN_EXPRESS_FILE)
+    await decompressAdminExpressFiles()
+  } else {
+    await downloadSourceFile(OSM_COMMUNES_BASE_URL + OSM_COMMUNES_FILE, OSM_COMMUNES_FILE)
+    await decompressOsmCommunesComFiles(OSM_COMMUNES_FILE, 'communes')
+  }
+  await downloadSourceFile(OSM_COMMUNES_BASE_URL + OSM_COMMUNES_COM_FILE, OSM_COMMUNES_COM_FILE)
+  await decompressOsmCommunesComFiles(OSM_COMMUNES_COM_FILE, 'communes-com')
 }
 
 main().catch(error => {

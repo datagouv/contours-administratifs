2df59f79b730c26b9e724640b8045872d59635f7
tmerlier <theophile.merliere@gmail.com>
2022-04-04 16:24:28 +0200

    Add generate ids for regions, departments and communes tiles

--- generate-mbtiles.sh
+++ generate-mbtiles.sh
@@ -9,13 +9,13 @@ echo "Génération des tuiles vectorielles découpage administratif"
 mkdir -p dist
 
 #echo "Régions"
-tippecanoe -l regions --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z3 -z12  -f -o dist/regions.mbtiles sources/regions-5m.geojson.gz
+tippecanoe -l regions --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z3 -z12  -f -o dist/regions.mbtiles sources/regions-5m.geojson.gz
 
 #echo "Départements"
-tippecanoe -l departements --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z3 -z12 -f -o dist/departements.mbtiles sources/departements-5m.geojson.gz
+tippecanoe -l departements --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z3 -z12 -f -o dist/departements.mbtiles sources/departements-5m.geojson.gz
 
 echo "Communes"
-tippecanoe -l communes --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z8 -z12 -f -o dist/communes.mbtiles sources/communes-5m.geojson.gz
+tippecanoe -l communes --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z8 -z12 -f -o dist/communes.mbtiles sources/communes-5m.geojson.gz
 
 echo "Merge des tuiles vectorielles"
 tile-join --attribution=Etalab --name=decoupage-administratif --no-tile-size-limit --no-tile-stats -f --output dist/decoupage-administratif.mbtiles dist/communes.mbtiles dist/departements.mbtiles dist/regions.mbtiles

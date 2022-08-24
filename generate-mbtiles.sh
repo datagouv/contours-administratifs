9eee30636aa118e8de1d3cc92f279e7a5afae396
Thomas Gratier <thomas_gratier@yahoo.fr>
2022-08-24 11:11:09 +0200

    Update to add mairies as layer in vector tiles

--- generate-mbtiles.sh
+++ generate-mbtiles.sh
@@ -4,20 +4,24 @@ mkdir -p sources
 wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/communes-5m.geojson.gz
 wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/departements-5m.geojson.gz
 wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/regions-5m.geojson.gz
+wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/chflieux-communes-arrondissements-municipaux.geojson.gz
 
 echo "Génération des tuiles vectorielles découpage administratif"
 mkdir -p dist
 
-#echo "Régions"
+echo "Régions"
 tippecanoe -l regions --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z3 -z12  -f -o dist/regions.mbtiles sources/regions-5m.geojson.gz
 
-#echo "Départements"
+echo "Départements"
 tippecanoe -l departements --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z3 -z12 -f -o dist/departements.mbtiles sources/departements-5m.geojson.gz
 
 echo "Communes"
 tippecanoe -l communes --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z8 -z12 -f -o dist/communes.mbtiles sources/communes-5m.geojson.gz
 
+echo "Mairies"
+tippecanoe -l mairies --generate-ids --no-tile-stats -Z8 -z12 -f -o dist/mairies.mbtiles sources/chflieux-communes-arrondissements-municipaux.geojson.gz
+
 echo "Merge des tuiles vectorielles"
-tile-join --attribution=Etalab --name=decoupage-administratif --no-tile-size-limit --no-tile-stats -f --output dist/decoupage-administratif.mbtiles dist/communes.mbtiles dist/departements.mbtiles dist/regions.mbtiles
+tile-join --attribution=Etalab --name=decoupage-administratif --no-tile-size-limit --no-tile-stats -f --output dist/decoupage-administratif.mbtiles dist/mairies.mbtiles dist/communes.mbtiles dist/departements.mbtiles dist/regions.mbtiles
 
 echo "Terminé"

f2e0be52f69e7c9b72a53d56611140dbf9ec82b6
Thomas Gratier <thomas_gratier@yahoo.fr>
2022-09-29 23:29:45 +0200

    Add EPCI in mbtiles

--- generate-mbtiles.sh
+++ generate-mbtiles.sh
@@ -4,6 +4,7 @@ mkdir -p sources
 wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/communes-5m.geojson.gz
 wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/departements-5m.geojson.gz
 wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/regions-5m.geojson.gz
+wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/epci-5m.geojson.gz
 wget -P sources -N http://etalab-datasets.geo.data.gouv.fr/contours-administratifs/latest/geojson/mairies.geojson.gz
 
 echo "Génération des tuiles vectorielles découpage administratif"
@@ -15,6 +16,9 @@ tippecanoe -l regions --generate-ids --no-tile-stats --drop-densest-as-needed --
 echo "Départements"
 tippecanoe -l departements --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z3 -z12 -f -o dist/departements.mbtiles sources/departements-5m.geojson.gz
 
+echo "EPCI"
+tippecanoe -l epcis --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z3 -z12 -f -o dist/epcis.mbtiles sources/epci-5m.geojson.gz
+
 echo "Communes"
 tippecanoe -l communes --generate-ids --no-tile-stats --drop-densest-as-needed --detect-shared-borders -Z8 -z12 -f -o dist/communes.mbtiles sources/communes-5m.geojson.gz
 
@@ -22,6 +26,6 @@ echo "Mairies"
 tippecanoe -l mairies --generate-ids --no-tile-stats -r1 -Z8 -z12 -f -o dist/mairies.mbtiles sources/mairies.geojson.gz
 
 echo "Merge des tuiles vectorielles"
-tile-join --attribution=Etalab --name=decoupage-administratif --no-tile-size-limit --no-tile-stats -f --output dist/decoupage-administratif.mbtiles dist/mairies.mbtiles dist/communes.mbtiles dist/departements.mbtiles dist/regions.mbtiles
+tile-join --attribution=Etalab --name=decoupage-administratif --no-tile-size-limit --no-tile-stats -f --output dist/decoupage-administratif.mbtiles dist/mairies.mbtiles dist/communes.mbtiles dist/epcis.mbtiles dist/departements.mbtiles dist/regions.mbtiles
 
 echo "Terminé"
